ca9f92fab62c30af4a12e21627915b22c5262422
Simon Widjaja <mail@simonwidjaja.com>
2016-11-12 08:52:50 -0800

    Templates added

---
+++ templates/AnimateCommons_Default/AnimateCommons_Default.js
@@ -0,0 +1,78 @@
+(function (lib, img, cjs, ss, an) {
+
+var p; // shortcut to reference prototypes
+lib.ssMetadata = [];
+
+
+// symbols:
+// helper functions:
+
+function mc_symbol_clone() {
+	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop));
+	clone.gotoAndStop(this.currentFrame);
+	clone.paused = this.paused;
+	clone.framerate = this.framerate;
+	return clone;
+}
+
+function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
+	var prototype = cjs.extend(symbol, cjs.MovieClip);
+	prototype.clone = mc_symbol_clone;
+	prototype.nominalBounds = nominalBounds;
+	prototype.frameBounds = frameBounds;
+	return prototype;
+	}
+
+
+(lib.Symbol1 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// Layer 1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#0066CC").s().p("Aj5ElQhthtAAibQAAiZBthtQBthtCaAAQCaAABtBtQBtBtglBnQglBmilAgQijAgg+CAQghBGgwAAQgoAAgygyg");
+	this.shape.setTransform(35.9,34.3);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.Symbol1, new cjs.Rectangle(0,0,71.8,68.6), null);
+
+
+// stage content:
+(lib.AnimateCommons_Default = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// timeline functions:
+	this.frame_0 = function() {
+		this.btn.on('click', function() {
+			AC.Debug.enableFpsMeter(this);
+		});
+	}
+
+	// actions tween:
+	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(1));
+
+	// Layer 1
+	this.btn = new lib.Symbol1();
+	this.btn.parent = this;
+	this.btn.setTransform(175,57.3,1,1,0,0,0,35.9,34.3);
+
+	this.timeline.addTween(cjs.Tween.get(this.btn).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(439.1,223,71.8,68.6);
+// library properties:
+lib.properties = {
+	width: 600,
+	height: 400,
+	fps: 30,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [],
+	preloads: []
+};
+
+
+
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{}, ss = ss||{}, AdobeAn = AdobeAn||{});
+var lib, images, createjs, ss, AdobeAn;
